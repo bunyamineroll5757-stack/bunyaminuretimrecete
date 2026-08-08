07fff359ab6018f0392679af96b601ffb1ecb23a
bunyamineroll5757-stack <bunyamineroll.5757@gmail.com>
2026-08-08 07:10:48 +0300

    Add files via upload

---
+++ js/supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://ihsetoxxggiaeulpqhzg.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_HoqAcz7S0ANBNyQTj6fjew_YCa_71nb"
+
+window.supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
