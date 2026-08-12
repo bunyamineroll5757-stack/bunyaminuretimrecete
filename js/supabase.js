1f69e818385a715396ea74e59d0b540eca25d761
bunyamineroll5757-stack <bunyamineroll.5757@gmail.com>
2026-08-13 00:13:20 +0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,8 +1,10 @@
+// Supabase Bilgileri
 const SUPABASE_URL = "https://ihsetoxxggiaeulpqhzg.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_HoqAcz7S0ANBNyQTj6fjew_YCa_71nb"
 
-const SUPABASE_KEY = "sb_publishable_HoqAcz7S0ANBNyQTj6fjew_YCa_71nb"
-
-window.supabaseClient = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
+// İstemciyi doğrudan küresel window.sbClient nesnesine atıyoruz
+if (typeof supabase !== 'undefined') {
+    window.sbClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+} else {
+    console.error("Supabase SDK kütüphanesi yüklenemedi!");
+}
