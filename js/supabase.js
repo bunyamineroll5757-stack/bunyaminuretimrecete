62edcdc0d7699d64616525cd025bbbee4cf47984
bunyamineroll5757-stack <bunyamineroll.5757@gmail.com>
2026-08-15 09:01:49 +0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -4,7 +4,7 @@ const SUPABASE_ANON_KEY = "sb_publishable_HoqAcz7S0ANBNyQTj6fjew_YCa_71nb"
 
 // İstemciyi doğrudan küresel window.sbClient nesnesine atıyoruz
 if (typeof supabase !== 'undefined') {
-    window.sbClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+   window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 } else {
     console.error("Supabase SDK kütüphanesi yüklenemedi!");
 }
